4e5b128e86a3d9ab1570b0ae2f0204a85aa035bd
nemolovich <nemolovich@hotmail.fr>
2013-10-30 18:31:03 +0100

    Fixed MainClient

--- src/client/main/ClientMain.java
+++ src/client/main/ClientMain.java
@@ -7,14 +7,9 @@ import java.rmi.ConnectException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.util.List;
-import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
-import server.objects.Message;
-import server.objects.interfaces.DiscussionSubjectInterface;
-import server.objects.interfaces.MessageInterface;
 import server.objects.interfaces.ServerForumInterface;
 import client.gui.GetStringDialog;
 import client.implementation.ClientDisplayer;
@@ -37,7 +32,7 @@ public class ClientMain {
 	 * @param cd {@link ClientDisplayer} - The client displayer
 	 * @throws RemoteException
 	 */
-//	@SuppressWarnings("resource")
+	@SuppressWarnings("resource")
 	public void start(ClientDisplayerInterface cd) throws RemoteException {
 
 		if(cd==null) {
@@ -93,181 +88,181 @@ public class ClientMain {
 			 */
 			cd.getMainFrame().updateSubjectPanel(server.getDiscussions());
 
-			String help="Available commands:\n" +
-					"\t/help:\t\t\tDisplay commands\n" +
-					"\t/create <channel>:\tCreate the channel <channel> on the server\n" +
-					"\t/exit:\t\t\tLeave client\n" +
-					"\t/list:\t\t\tDisplay channels list\n" +
-					"\t/messages:\t\tDisplay the current discussion messages\n" +
-					"\t/say <message>:\tAdd the message <message> from you on your current channel\n" +
-					"\t/subscribe <channel>:\tSuscribe to channel <channel>\n" +
-					"\t/switch <channel>:\tSwitch to channel <channel>";
+//			String help="Available commands:\n" +
+//					"\t/help:\t\t\tDisplay commands\n" +
+//					"\t/create <channel>:\tCreate the channel <channel> on the server\n" +
+//					"\t/exit:\t\t\tLeave client\n" +
+//					"\t/list:\t\t\tDisplay channels list\n" +
+//					"\t/messages:\t\tDisplay the current discussion messages\n" +
+//					"\t/say <message>:\tAdd the message <message> from you on your current channel\n" +
+//					"\t/subscribe <channel>:\tSuscribe to channel <channel>\n" +
+//					"\t/switch <channel>:\tSwitch to channel <channel>";
 
 //			cd.display(help, true);
 
-			DiscussionSubjectInterface currentDiscussion=null;
-			Scanner sc=new Scanner(System.in);
-			do {
-				command=sc.nextLine();
-				if(command.toLowerCase().startsWith("/help")) {
-					cd.display(help, true);
-				}
-				else if(command.toLowerCase().startsWith("/create")) {
-					if(server.isFullChannel(cd)) {
-						cd.error("You can not create more channel, delete one before", true);
-						continue;
-					}
-					int len="/create".length();
-					if(command.length()<="/create".length()+1) {
-						cd.error("Please specify a message", true);
-						continue;
-					}
-					String subject=command.substring(len+1,command.length());
-					if(subject.endsWith(" ")) {
-						subject=subject.substring(0,subject.lastIndexOf(" "));
-					}
-					if(server.getSubjectFromName(subject)!=null) {
-						cd.error("The channel '"+subject+"' already exists", true);
-						continue;
-					}
-					DiscussionSubjectInterface dsi = server.create(cd,subject);
-					if(dsi!=null) {
-						currentDiscussion=dsi;
-						cd.display("The channel '"+subject+"' has been correctly " +
-								"created", true);
-					}
-					else {
-						cd.error("The channel '"+subject+"' can not be created",
-								true);
-					}
-				}
-				else if(command.toLowerCase().startsWith("/list")) {
-					if(server.getDiscussions().isEmpty()) {
-						cd.error("There is no discussion on the forum", true);
-					}
-					String list="Discussions list:\n";
-					for(DiscussionSubjectInterface dsi:server.getDiscussions()) {
-						list+="\t\t"+dsi.getTitle()+(dsi.isConnected(cd.getClient())?
-								" *":"")+"\n";
-					}
-					cd.display(list, false);
-					cd.getMainFrame().updateSubjectPanel(server.getDiscussions());
-				}
-				else if(command.toLowerCase().startsWith("/messages")) {
-					if(currentDiscussion==null) {
-						cd.error("You don't have current discussion", true);
-					}
-					else {
-						List<MessageInterface> messages=currentDiscussion.getMessages();
-						for(MessageInterface msg:messages) {
-							cd.display(msg.getDateString()+msg.getClient().getPseudo()+
-									": "+msg.getMessage(), false);
-						}
-					}
-				}
-				else if(command.toLowerCase().startsWith("/remove")) {
-					int len="/remove".length();
-					if(command.length()<="/remove".length()+1) {
-						cd.error("Please specify a message", true);
-						continue;
-					}
-					String subject=command.substring(len+1,command.length());
-					if(subject.endsWith(" ")) {
-						subject=subject.substring(0,subject.lastIndexOf(" "));
-					}
-					if(!server.isChannelOwner(cd, subject)) {
-						cd.error("You are not the channel owner", true);
-						continue;
-					}
-					DiscussionSubjectInterface dsi=server.remove(cd, subject);
-					if(dsi!=null) {
-						if(currentDiscussion.equals(dsi)) {
-							currentDiscussion=dsi;
-						}
-						cd.display("The channel '"+subject+"' has been correctly " +
-								"removed", true);
-					}
-					else {
-						cd.error("The channel '"+subject+"' can not be remove", true);
-					}
-				}
-				else if(command.toLowerCase().startsWith("/say")) {
-					int len="/say".length();
-					if(command.length()<="/say".length()+1) {
-						cd.error("Please specify a message", true);
-						continue;
-					}
-					String message=command.substring(len+1,command.length());
-					if(message.endsWith(" ")) {
-						message=message.substring(0,message.lastIndexOf(" "));
-					}
-					if(currentDiscussion==null) {
-						cd.error("You don't have current discussion", true);
-					}
-					else {
-						currentDiscussion.addMessage(
-								new Message(cd.getClient(), message));
-					}
-
-				}
-				else if(command.toLowerCase().startsWith("/subscribe")) {
-					if(command.length()<="/subscribe".length()+1) {
-						cd.error("Please specify the channel name", true);
-						continue;
-					}
-					int len="/subscribe".length();
-					String subject=command.substring(len+1,command.length());
-					if(subject.endsWith(" ")) {
-						subject=subject.substring(0,subject.lastIndexOf(" "));
-					}
-					cd.display("Subscribe to: "+subject, false);
-					DiscussionSubjectInterface dsi=server.getSubjectFromName(subject);
-					if(dsi!=null) {
-						if(server.subscribe(dsi,cd)) {
-							cd.display("You are now connected to '"+dsi.getTitle()+
-									"' channel", true);
-							currentDiscussion=dsi;
-						}
-						else {
-							cd.error("You failed to connect to '"+dsi.getTitle()+
-									"' channel", true);
-						}
-					}
-					else {
-						cd.error("The channel '"+subject+"' has not been found", true);
-					}
-				}
-				else if(command.toLowerCase().startsWith("/switch")) {
-					if(command.length()<="/switch".length()+1) {
-						cd.error("Please specify the channel name", true);
-						continue;
-					}
-					int len="/switch".length();
-					String subject=command.substring(len+1,command.length());
-					if(subject.endsWith(" ")) {
-						subject=subject.substring(0,subject.lastIndexOf(" "));
-					}
-					cd.display("Switch to: "+subject, false);
-					DiscussionSubjectInterface dsi=server.getSubjectFromName(subject);
-					if(dsi!=null) {
-						if(dsi.isConnected(cd.getClient())) {
-							cd.display("You switched to '"+dsi.getTitle()+
-									"' channel", false);
-							currentDiscussion=dsi;
-						}
-						else {
-							cd.error("You did not subscribe to '"+dsi.getTitle()+
-									"' channel", true);
-						}
-					}
-					else {
-						cd.error("The channel '"+subject+"' has not been found",
-								true);
-						command="";
-					}
-				}
-			} while(!command.equalsIgnoreCase("/exit"));
-			cd.exit();
+//			DiscussionSubjectInterface currentDiscussion=null;
+//			Scanner sc=new Scanner(System.in);
+//			do {
+//				command=sc.nextLine();
+//				if(command.toLowerCase().startsWith("/help")) {
+//					cd.display(help, true);
+//				}
+//				else if(command.toLowerCase().startsWith("/create")) {
+//					if(server.isFullChannel(cd)) {
+//						cd.error("You can not create more channel, delete one before", true);
+//						continue;
+//					}
+//					int len="/create".length();
+//					if(command.length()<="/create".length()+1) {
+//						cd.error("Please specify a message", true);
+//						continue;
+//					}
+//					String subject=command.substring(len+1,command.length());
+//					if(subject.endsWith(" ")) {
+//						subject=subject.substring(0,subject.lastIndexOf(" "));
+//					}
+//					if(server.getSubjectFromName(subject)!=null) {
+//						cd.error("The channel '"+subject+"' already exists", true);
+//						continue;
+//					}
+//					DiscussionSubjectInterface dsi = server.create(cd,subject);
+//					if(dsi!=null) {
+//						currentDiscussion=dsi;
+//						cd.display("The channel '"+subject+"' has been correctly " +
+//								"created", true);
+//					}
+//					else {
+//						cd.error("The channel '"+subject+"' can not be created",
+//								true);
+//					}
+//				}
+//				else if(command.toLowerCase().startsWith("/list")) {
+//					if(server.getDiscussions().isEmpty()) {
+//						cd.error("There is no discussion on the forum", true);
+//					}
+//					String list="Discussions list:\n";
+//					for(DiscussionSubjectInterface dsi:server.getDiscussions()) {
+//						list+="\t\t"+dsi.getTitle()+(dsi.isConnected(cd.getClient())?
+//								" *":"")+"\n";
+//					}
+//					cd.display(list, false);
+//					cd.getMainFrame().updateSubjectPanel(server.getDiscussions());
+//				}
+//				else if(command.toLowerCase().startsWith("/messages")) {
+//					if(currentDiscussion==null) {
+//						cd.error("You don't have current discussion", true);
+//					}
+//					else {
+//						List<MessageInterface> messages=currentDiscussion.getMessages();
+//						for(MessageInterface msg:messages) {
+//							cd.display(msg.getDateString()+msg.getClient().getPseudo()+
+//									": "+msg.getMessage(), false);
+//						}
+//					}
+//				}
+//				else if(command.toLowerCase().startsWith("/remove")) {
+//					int len="/remove".length();
+//					if(command.length()<="/remove".length()+1) {
+//						cd.error("Please specify a message", true);
+//						continue;
+//					}
+//					String subject=command.substring(len+1,command.length());
+//					if(subject.endsWith(" ")) {
+//						subject=subject.substring(0,subject.lastIndexOf(" "));
+//					}
+//					if(!server.isChannelOwner(cd, subject)) {
+//						cd.error("You are not the channel owner", true);
+//						continue;
+//					}
+//					DiscussionSubjectInterface dsi=server.remove(cd, subject);
+//					if(dsi!=null) {
+//						if(currentDiscussion.equals(dsi)) {
+//							currentDiscussion=dsi;
+//						}
+//						cd.display("The channel '"+subject+"' has been correctly " +
+//								"removed", true);
+//					}
+//					else {
+//						cd.error("The channel '"+subject+"' can not be remove", true);
+//					}
+//				}
+//				else if(command.toLowerCase().startsWith("/say")) {
+//					int len="/say".length();
+//					if(command.length()<="/say".length()+1) {
+//						cd.error("Please specify a message", true);
+//						continue;
+//					}
+//					String message=command.substring(len+1,command.length());
+//					if(message.endsWith(" ")) {
+//						message=message.substring(0,message.lastIndexOf(" "));
+//					}
+//					if(currentDiscussion==null) {
+//						cd.error("You don't have current discussion", true);
+//					}
+//					else {
+//						currentDiscussion.addMessage(
+//								new Message(cd.getClient(), message));
+//					}
+//
+//				}
+//				else if(command.toLowerCase().startsWith("/subscribe")) {
+//					if(command.length()<="/subscribe".length()+1) {
+//						cd.error("Please specify the channel name", true);
+//						continue;
+//					}
+//					int len="/subscribe".length();
+//					String subject=command.substring(len+1,command.length());
+//					if(subject.endsWith(" ")) {
+//						subject=subject.substring(0,subject.lastIndexOf(" "));
+//					}
+//					cd.display("Subscribe to: "+subject, false);
+//					DiscussionSubjectInterface dsi=server.getSubjectFromName(subject);
+//					if(dsi!=null) {
+//						if(server.subscribe(dsi,cd)) {
+//							cd.display("You are now connected to '"+dsi.getTitle()+
+//									"' channel", true);
+//							currentDiscussion=dsi;
+//						}
+//						else {
+//							cd.error("You failed to connect to '"+dsi.getTitle()+
+//									"' channel", true);
+//						}
+//					}
+//					else {
+//						cd.error("The channel '"+subject+"' has not been found", true);
+//					}
+//				}
+//				else if(command.toLowerCase().startsWith("/switch")) {
+//					if(command.length()<="/switch".length()+1) {
+//						cd.error("Please specify the channel name", true);
+//						continue;
+//					}
+//					int len="/switch".length();
+//					String subject=command.substring(len+1,command.length());
+//					if(subject.endsWith(" ")) {
+//						subject=subject.substring(0,subject.lastIndexOf(" "));
+//					}
+//					cd.display("Switch to: "+subject, false);
+//					DiscussionSubjectInterface dsi=server.getSubjectFromName(subject);
+//					if(dsi!=null) {
+//						if(dsi.isConnected(cd.getClient())) {
+//							cd.display("You switched to '"+dsi.getTitle()+
+//									"' channel", false);
+//							currentDiscussion=dsi;
+//						}
+//						else {
+//							cd.error("You did not subscribe to '"+dsi.getTitle()+
+//									"' channel", true);
+//						}
+//					}
+//					else {
+//						cd.error("The channel '"+subject+"' has not been found",
+//								true);
+//						command="";
+//					}
+//				}
+//			} while(!command.equalsIgnoreCase("/exit"));
+//			cd.exit();
 		} catch (ConnectException e) {
 			if(this.tries<5) {
 				int wait=this.tries*5;
