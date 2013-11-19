dffd430695a0b0c82afbaeb837b4218e6e884f40
nemolovich <nemolovich@hotmail.fr>
2013-11-19 16:46:51 +0100

    First corrections

--- src/server/objects/ServerRemoteForum.java
+++ src/server/objects/ServerRemoteForum.java
@@ -1,6 +1,8 @@
 package server.objects;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -16,9 +18,18 @@ public class ServerRemoteForum extends ServerForum {
 	private static final long serialVersionUID = 6541361995114344327L;
 	private String remoteURL = null;
 
-	public ServerRemoteForum(String url, int port,
+	public ServerRemoteForum(int port, String subject,
 			List<ClientDisplayerInterface> clients) throws RemoteException {
 		super();
+
+		String url = null;
+		try {
+			url = "//" + InetAddress.getLocalHost().getHostAddress() + ":"
+					+ port + "/"
+					+ subject;
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
 		this.remoteURL = url;
 		this.clients = clients;
 		System.out.println("[ Registry of object with URL  ] : " + url);
